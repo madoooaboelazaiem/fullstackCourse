7dc77dba6fb2a6c4dc89ed7ceb0fb15008e7700c
madoooaboelazaiem <madooo1998@hotmail.com>
2020-10-11 22:08:35 +0200

    added header and footer

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import { Dishes } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -20,11 +21,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <div className="container">
           <Menu
             dishes={this.state.dishes}
@@ -38,6 +35,7 @@ class Main extends Component {
             }
           />
         </div>
+        <Footer />
       </div>
     );
   }
